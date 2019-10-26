c30ebc063706bd0627b6a35c8d82715749a3c6f0
Ossiggy <mrossig201@gmail.com>
2019-10-26 11:17:24 -0400

    added validation for PUT routes and fixed 404 bug for GET routes

--- controllers/mlbController.js
+++ controllers/mlbController.js
@@ -1,65 +1,77 @@
-'use strict';
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { Game, mlbFields } = require('../models');
-const { compareDate, isValid } = require('../helpers');
-const { mlbService } = require('../services');
+"use strict";
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { Game, mlbFields } = require("../models");
+const { compareDate, isValid } = require("../helpers");
+const { mlbService } = require("../services");
 
 const router = express.Router();
 const jsonParser = bodyParser.json();
 
 mongoose.Promise = global.Promise;
 
-router.get('/:id', (req, res) => {
-  if(!Game) { 
-    res.status(404).json({error: 'Game not found'});
-  }
-  else {
-    Game.findById(req.params.id)
-      .then(async (game) => {
-          if (compareDate(game.updatedAt)){
-            console.log('over 15 second mark', game.feedUrl);
-            const newGame = await mlbService.returnUpdated({
-              id: req.params.id,
-              feed: game.feedUrl
-            });
-            return res.json(newGame);
-          }
-          console.log('under 15 second mark');
-          return res.json(game);
-        })
-      .catch(err => {
-        console.error(err);
-        return res.status(500).json({error: 'something went wrong', message: err.message});
-      });
-  };
+router.get("/:id", (req, res) => {
+  Game.findById(req.params.id)
+    .then(async game => {
+      if (!game) {
+        return res.status(404).json({ error: "Game not found" });
+      }
+      if (compareDate(game.updatedAt)) {
+        console.log("over 15 second mark", game.feedUrl);
+        const newGame = await mlbService.returnUpdated({
+          id: req.params.id,
+          feed: game.feedUrl
+        });
+        return res.json(newGame);
+      }
+      console.log("under 15 second mark");
+      return res.json(game);
+    })
+    .catch(err => {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: "something went wrong", message: err.message });
+    });
 });
 
-router.post('/', jsonParser, (req, res) => {
+router.post("/", jsonParser, (req, res) => {
   const { body } = req;
-  if (!isValid(body, mlbFields) || body.league !== 'MLB'){
-    return res.status(403).json({ message: 'format invalid, expected MLB fields', error: err.message })
+  if (!isValid(body, mlbFields) || body.league !== "MLB") {
+    return res.status(403).json({
+      message: "format invalid, expected MLB fields",
+      error: err.message
+    });
   }
 
   const game = mlbService.create(req.body);
 
-  Game
-    .create(game, (err, game) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Internal server error', error: err.message });
-      }
-      return res.status(201).json(game);
-    });
+  Game.create(game, (err, game) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: err.message });
+    }
+    return res.status(201).json(game);
+  });
 });
 
-router.put('/:id', jsonParser, async (req, res) => {
+router.put("/:id", jsonParser, async (req, res) => {
+  const { body, params } = req;
+  if (!isValid(body, mlbFields) || body.league !== "MLB") {
+    return res.status(403).json({
+      message: "format invalid, expected MLB fields",
+      error: err.message
+    });
+  }
+
   try {
-    const game = await mlbService.update({ id: req.params.id, data: req.body});
+    const game = await mlbService.update({ id: params.id, data: body });
     return res.status(201).json(game);
   } catch (err) {
-    return res.status(500).json({ message: 'Internal server error' });
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
