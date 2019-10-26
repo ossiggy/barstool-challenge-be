e4aa5090c09eac6f03c5a9bdc438ca1e7d9532fe
Ossiggy <mrossig201@gmail.com>
2019-10-26 11:17:38 -0400

    removed console.log

--- helpers/compareDate.js
+++ helpers/compareDate.js
@@ -6,7 +6,6 @@ const compareDate = (prev) => {
   const oldStamp = timestamp.fromDate(prev);
   const current = timestamp.now();
   const difference = current - oldStamp;
-  console.log(oldStamp, current, difference);
   if (difference >= 15) {
     return true;
   }
